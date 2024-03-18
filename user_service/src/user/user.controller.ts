877d0ad6d88f88805de96027baa8ea0424a9d20e
iurie.balan <iurie.balan@112.md>
2024-03-18 15:02:51 +0200

    removed logger

--- user_service/src/user/user.controller.ts
+++ user_service/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Logger } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import {
   GetUserRequest,
   User,
@@ -9,9 +9,7 @@ import {
 @Controller()
 @UserServiceControllerMethods()
 export class UserController implements UserServiceController {
-  private readonly logger = new Logger(UserController.name);
   getUser(request: GetUserRequest): Promise<User> {
-    this.logger.log(request);
     // Implement your logic to retrieve the item based on the request
     // You can use the request.itemId to fetch the specific item from your data source
     console.log(request);
